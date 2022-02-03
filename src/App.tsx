735c21bd915e06b5e9d3d770967aa6511d63750c
Abubakr Abduvakhidov <abubakr@Abubakrs-MacBook-Pro.local>
2022-02-04 00:36:08 +0500

    Implement cards grid, footer styling

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,31 @@ import {
     Grid,
     Toolbar,
     Container,
-    Button
+    Button,
+    CardContent
 } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
 
+import useStyles from './styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const App = () => {
+    const classes = useStyles();
+
     return (
         <>
             <CssBaseline />
             <AppBar position="relative">
                 <Toolbar>
-                    <PhotoCamera />
+                    <PhotoCamera className={classes.icon}/>
                     <Typography variant="h6">
                         Photo Album
                     </Typography>
                 </Toolbar>
             </AppBar>
             <main>
-                <div>
+                <div className={classes.container}>
                     <Container maxWidth="sm">
                         <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                             Photo Album
@@ -34,23 +41,59 @@ const App = () => {
                         <Typography variant="h5" align="center" color="textSecondary" paragraph>
                             Hello everyone This is a photo album and I'm trying to make it as long as possible so that it looks long and beautiful.
                         </Typography>
-                        <div>
+                        <div className={classes.buttons}>
                             <Grid container spacing={2} justify="center">
-                                <Grid item>
-                                    <Button variant="contained" color="primary">
-                                        See my photos
-                                    </Button>
-                                </Grid>
-                                <Grid item>
-                                    <Button variant="outlined" color="secondary">
-                                        Secondary Action
-                                    </Button>
-                                </Grid>
+                                    <Grid item>
+                                        <Button variant="contained" color="primary">
+                                            See my photos
+                                        </Button>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="outlined" color="secondary">
+                                            Secondary Action
+                                        </Button>
+                                    </Grid>
                             </Grid>
                         </div>
                     </Container>
                 </div>
+                <Container className={classes.cardGrid} maxWidth="md">
+                    <Grid container spacing={4}>
+                    {cards.map((card) => (
+
+                        <Grid item key={card} xs={12} sm={6} md={4}>
+                            <Card className={classes.card}>
+                                <CardMedia 
+                                    className={classes.cardMedia}
+                                    image="https://source.unsplash.com/random"
+                                    title="Image Title"
+                                />
+                                <CardContent className={classes.cardContent}>
+                                    <Typography gutterBottom variant="h6">
+                                        Heading
+                                    </Typography>
+                                    <Typography>
+                                        This is a media card. You can use this section to describe the content.
+                                    </Typography>
+                                    <CardActions>
+                                        <Button size="small" color="primary">View</Button>
+                                        <Button size="small" color="primary">Edit</Button>
+                                    </CardActions>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                    </Grid>
+                </Container>
             </main>
+            <footer className={classes.footer}>
+                <Typography variant="h6" align="center" gutterBottom>
+                    Footer
+                </Typography>
+                <Typography variant="subtitle1" align="center" color="textSecondary">
+                    Something here to give the footer look.
+                </Typography>
+            </footer>
         </>
     )
 }
